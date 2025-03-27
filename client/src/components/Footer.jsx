1d94d4a78e75a9c4b926ff2e69ef6c21fc86a732
indreshverma2004 <147939015+indreshverma2004@users.noreply.github.com>
2025-03-27 18:38:11 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
     return (
         <footer className="footer">
             <div className="footer-bottom">
-                <p>&copy; {new Date().getFullYear()} Smart Parking System. All Rights Reserved.</p>
+                <p>&copy; {new Date().getFullYear()} Smart Parking System. All Rights are Reserved.</p>
             </div>
         </footer>
     );
